d9b6fccf6d3050a89428e5e275c8b7b69c2cf58b
DADDesign-Projects <p.bantigny@gmail.com>
2026-01-03 21:15:35 +0100

    Adding a phasing effect to the modulations

--- Effects/Modulations/Src/cModulations.cpp
+++ Effects/Modulations/Src/cModulations.cpp
@@ -11,6 +11,7 @@
 #include "cChorus.h"
 #include "cFlanger.h"
 #include "cUniVibe.h"
+#include "cPhaser.h"
 
 namespace DadEffect {
 //**********************************************************************************
@@ -23,6 +24,7 @@ alignas(cTremoloVibrato) uint8_t TremoloVibratoBuffer[sizeof(cTremoloVibrato)];
 alignas(cChorus) uint8_t ChorusBuffer[sizeof(cChorus)];
 alignas(cFlanger) uint8_t FlangerBuffer[sizeof(cFlanger)];
 alignas(cUniVibe) uint8_t UniVibeBuffer[sizeof(cUniVibe)];
+alignas(cPhaser) uint8_t PhaserBuffer[sizeof(cPhaser)];
 
 // ---------------------------------------------------------------------------------
 // Initialize
@@ -30,10 +32,11 @@ alignas(cUniVibe) uint8_t UniVibeBuffer[sizeof(cUniVibe)];
 // ---------------------------------------------------------------------------------
 void cMainModulations::Initialize(){
 	// Create effect instances using placement new in pre-allocated buffers
-	m_TabEffects[3] = new (TremoloVibratoBuffer) cTremoloVibrato();
+	m_TabEffects[0] = new (PhaserBuffer) cPhaser();
+	m_TabEffects[1] = new (TremoloVibratoBuffer) cTremoloVibrato();
 	m_TabEffects[2] = new (ChorusBuffer) cChorus();
-	m_TabEffects[1] = new (FlangerBuffer) cFlanger();
-	m_TabEffects[0] = new (UniVibeBuffer) cUniVibe();
+	m_TabEffects[3] = new (FlangerBuffer) cFlanger();
+	m_TabEffects[4] = new (UniVibeBuffer) cUniVibe();
 
 	// Configure multi-mode effect system
 	m_NbEffects = NB_EFFECTS;                              // Set number of available effects
