436390fa5cb75df90c3cb3f5ef37608ee980a299
DADDesign-Projects <p.bantigny@gmail.com>
2026-01-07 15:12:14 +0100

    moves the calculation of biquad parameters to the background.

--- GUI/Components/Src/cPanelOfTone.cpp
+++ GUI/Components/Src/cPanelOfTone.cpp
@@ -29,9 +29,9 @@ void cPanelOfTone::Initialize(uint32_t SerializeID) {
     m_TrebleBiQuad.Initialize(SAMPLING_RATE, 6500, 0.0f, 1.8f, DadDSP::FilterType::HSH);
 
     // Initialize tone control filters
-    m_Bass.Init(  SerializeID, 0, 0.0f, -10.0f, +10.0f, 1.0f, 0.5f, BassCallback,   (uint32_t) this, 0, 100);
-    m_Mid.Init(   SerializeID, 0, 0.0f, -10.0f, +10.0f, 1.0f, 0.5f, MidCallback,    (uint32_t) this, 0, 101);
-    m_Treble.Init(SerializeID, 0, 0.0f, -10.0f, +10.0f, 1.0f, 0.5f, TrebleCallback, (uint32_t) this, 0, 102);
+    m_Bass.Init(  SerializeID, 0, 0.0f, -10.0f, +10.0f, 1.0f, 0.5f, 0, 0, 0, 100);
+    m_Mid.Init(   SerializeID, 0, 0.0f, -10.0f, +10.0f, 1.0f, 0.5f, 0, 0, 0, 101);
+    m_Treble.Init(SerializeID, 0, 0.0f, -10.0f, +10.0f, 1.0f, 0.5f, 0, 0, 0, 102);
 
     // Initialize tone view
     m_BassView.Init(&m_Bass, "Bass", "Bass", "dB", "dB");
@@ -56,9 +56,19 @@ void cPanelOfTone::addToSerializeFamily(uint32_t SerializeID) {
 // -----------------------------------------------------------------------------
 void cPanelOfTone::Update() {
     if (m_isActive) {
-    	//m_Bass.Process();
-    	//m_Mid.Process();
-    	//m_Treble.Process();
+    	if(m_Bass.getValue() != m_BassBiQuad.getGainDb()){
+    	    m_BassBiQuad.setGainDb(m_Bass.getValue());
+    	    m_BassBiQuad.CalculateParameters();
+    	}
+    	if(m_Mid.getValue() != m_MidBiQuad.getGainDb()){
+    	    m_MidBiQuad.setGainDb(m_Mid.getValue());
+    	    m_MidBiQuad.CalculateParameters();
+    	}
+    	if(m_Treble.getValue() != m_TrebleBiQuad.getGainDb()){
+    		m_TrebleBiQuad.setGainDb(m_Treble.getValue());
+    		m_TrebleBiQuad.CalculateParameters();
+    	}
+
         cPanelOfParameterView::Update(); // Update base class functionality
     }
 }
@@ -73,39 +83,15 @@ ITCM void cPanelOfTone::Process(AudioBuffer* pIn, AudioBuffer* pOut, eOnOff OnOf
 	OutLeft = m_BassBiQuad.Process(pIn->Left, DadDSP::eChannel::Left);
 	OutLeft = m_MidBiQuad.Process(OutLeft, DadDSP::eChannel::Left);
 	pOut->Left =  m_TrebleBiQuad.Process(OutLeft, DadDSP::eChannel::Left);
+	//pOut->Left =  m_BassBiQuad.Process(pIn->Left, DadDSP::eChannel::Left);
 
 	OutRight = m_BassBiQuad.Process(pIn->Right, DadDSP::eChannel::Right);
 	OutRight = m_MidBiQuad.Process(OutRight, DadDSP::eChannel::Right);
-	pOut->Right =  m_TrebleBiQuad.Process(OutRight, DadDSP::eChannel::Right);
+	pOut->Right = m_TrebleBiQuad.Process(OutRight, DadDSP::eChannel::Right);
+	//pOut->Right = m_BassBiQuad.Process(pIn->Right, DadDSP::eChannel::Right);
 
 }
 
-// -----------------------------------------------------------------------------
-// Callback for bass parameter changes
-// -----------------------------------------------------------------------------
-void cPanelOfTone::BassCallback(DadDSP::cParameter* pParameter, uint32_t Context){
-	cPanelOfTone *pthis = (cPanelOfTone *)Context;  // Get class instance
-    pthis->m_BassBiQuad.setGainDb(pParameter->getValue());
-    pthis->m_BassBiQuad.CalculateParameters();
-}
-
-// -----------------------------------------------------------------------------
-// Callback for mid parameter changes
-// -----------------------------------------------------------------------------
-void cPanelOfTone::MidCallback(DadDSP::cParameter* pParameter, uint32_t Context){
-	cPanelOfTone *pthis = (cPanelOfTone *)Context;  // Get class instance
-    pthis->m_MidBiQuad.setGainDb(pParameter->getValue());
-    pthis->m_MidBiQuad.CalculateParameters();
-}
-
-// -----------------------------------------------------------------------------
-// Callback for treble parameter changes
-// -----------------------------------------------------------------------------
-void cPanelOfTone::TrebleCallback(DadDSP::cParameter* pParameter, uint32_t Context){
-	cPanelOfTone *pthis = (cPanelOfTone *)Context;  // Get class instance
-    pthis->m_TrebleBiQuad.setGainDb(pParameter->getValue());
-    pthis->m_TrebleBiQuad.CalculateParameters();
-}
 
 } // namespace DadGUI
 //***End of file**************************************************************
