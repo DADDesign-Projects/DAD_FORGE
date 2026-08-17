1998569755a84881dbf0a99f477a887a11ef09e5
DADDesign-Projects <p.bantigny@gmail.com>
2026-08-17 14:12:32 +0200

    Allow effects to modify certain parameters

--- Inc/HardwareDefines.h
+++ Inc/HardwareDefines.h
@@ -6,17 +6,13 @@
 //
 // This file is part of the library and should not be modified directly.
 //
-// To customize the hardware configuration for a specific application, copy
-// this file into your project directory and rename it to
-// 'HardwareAndCoDefines.h'. The library will automatically use this file
-// instead of the default one.
 //
 // Copyright (c) 2026 DadDesign-Projects.
 //==============================================================================
 #pragma once
 
-#if __has_include("HardwareAndCoDefines.h")
-#include "HardwareAndCoDefines.h"
+#if __has_include("HardwareDefaultDefines.h")
+#include "HardwareDefaultDefines.h"
 #else
 #include "EffectsOptions.h"
 
